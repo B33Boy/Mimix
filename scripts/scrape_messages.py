bc0376a8e097b43f3509258cd505893dbf5ab4ac
B33Boy <abhip202@gmail.com>
2025-04-16 06:12:06 +0000

    configure logging and add typing info

--- scripts/scrape_messages.py
+++ scripts/scrape_messages.py
@@ -1,11 +1,14 @@
 import logging
+from typing import AsyncGenerator
 
 import discord
-from discord import Message
 from discord.ext import commands
+from discord.ext.commands import Context
 
 # Hacky fix to load the .env before the config
 from dotenv import load_dotenv
+
+from bot.logger import configure_logging
 
 load_dotenv()
 
@@ -16,38 +19,32 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = commands.Bot(command_prefix=PREFIX, intents=intents)
+configure_logging()
 
 LIMIT = 1000  # Total number of messages to search
-NUM_MESSAGES = 100  # Number of messages we want to collect
-AUTHORS = ["LlamaBot"]  # Messages with only these usernames will be read
+NUM_MESSAGES = 10  # Number of messages we want to collect
+AUTHORS = ["Mimix"]  # Messages with only these usernames will be read
+
 
 # ========================= Helpers =========================
-
-async def get_messages_from_channel(ctx, num_messages, authors=[]):
+async def get_messages_from_channel(ctx: Context, num_messages: int, authors: tuple[str] = []) -> AsyncGenerator[str, None]:
     counter = 0
     async for msg in ctx.channel.history(limit=LIMIT, oldest_first=False):
         if not authors or msg.author.name in authors:
             counter += 1
-            yield f" {counter} - {msg.content}"
+            yield f" {msg.author.name} - {counter} - {msg.content}"
 
         if counter >= num_messages:
             return
 
 # ========================= Events =========================
-
 @client.command()
-async def test(ctx, *args):
-    arguments = ', '.join(args)
-    await ctx.send(f'{len(args)} arguments: {arguments}')
-
-@client.command()
-async def scrape(ctx: commands.Context):
-    
+async def scrape(ctx: commands.Context) -> None:
     if ctx.author.name not in VALID_USERS:
         return
 
     async for msg in get_messages_from_channel(ctx, NUM_MESSAGES, AUTHORS):
-        logging.error(msg)
+        logging.info(msg)
 
 
 client.run(DISCORD_TOKEN)
